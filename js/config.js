8baea9425ea0caa5ea4caf300f2fcd7c5acfd4d5
Priyangka Roy <priyangka.roy085@gmail.com>
2025-04-25 13:28:23 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_zJwxPeuom', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5tkgsj1eqrs6tub4661rmac8pc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4k08eutfq179f1inujdst3fgo7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
