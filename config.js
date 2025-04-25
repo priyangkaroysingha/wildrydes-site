d52c62a790bccf0488b7ade224df5a5ae71fa011
Priyangka Roy <priyangka.roy085@gmail.com>
2025-04-25 11:36:05 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_QAxlp6Kux', // Replace with your actual pool ID
-        userPoolClientId: '2cnvgimv7lekfv6l8ujf3932nv', // Replace with your actual client ID
+        userPoolId: 'us-east-1_zJwxPeuom', // Replace with your actual pool ID
+        userPoolClientId: '5tkgsj1eqrs6tub4661rmac8pc', // Replace with your actual client ID
         region: 'us-east-1'
     },
     api: {
