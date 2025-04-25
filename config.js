2afe244de14316fc8406f995343c01a1794508a9
Priyangka Roy <priyangka.roy085@gmail.com>
2025-04-25 10:48:17 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_QAxlp6Kux', // Replace with your actual pool ID
+        userPoolClientId: '2cnvgimv7lekfv6l8ujf3932nv', // Replace with your actual client ID
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: '' // Leave this blank for now, will add API URL later
+    }
+};
